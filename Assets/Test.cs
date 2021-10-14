da53ff9b022ecc40b4a5e62c65c9c927570fd8a7
tora-ru <91678534+tora-ru@users.noreply.github.com>
2021-10-14 22:15:59 +0900

    lesson４課題　順に表示
    lesson４の課題
    要素を順に表示する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,7 +8,6 @@ public class Test : MonoBehaviour
     {
         //要素数の配列を初期化する
         int[] array = new int[5];
-
         //配列の各要素に値を代入する
         array[0] = 67;
         array[1] = 10101;
@@ -16,17 +15,20 @@ public class Test : MonoBehaviour
         array[3] = 99;
         array[4] = 77;
         // 配列の要素をすべて表示する
-        for (int i = 4; i >= 0; i--)
+        for (int i = 0; i < 5; i++)
         {
-
             Debug.Log(array[i]);
-
         }
-      
     }
     // Update is called once per frame
     void Update()
     {
     }
 }
+
+
+
+
+
+
 
