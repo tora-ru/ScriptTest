d31d9bbd610d552f299d26f31b870f301788f2a3
tora-ru <91678534+tora-ru@users.noreply.github.com>
2021-10-21 22:15:01 +0900

    逆順に表示(再提出)
    lesson４　課題　再提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,7 +9,7 @@ public class Test : MonoBehaviour
         //配列を初期化する
         int[] array = { 67, 10101, 77, 88, 99 };
         // 配列の要素をすべて表示する
-        for (int i = 0; i < array.Length; i++)
+        for (int i = array.Length-1; i >= 0; i--)
         {
             Debug.Log (array [i]);
         }
