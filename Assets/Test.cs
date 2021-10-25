ae0b46b21a22dbd3e5f8185408df7afdd141ebc3
tora-ru <91678534+tora-ru@users.noreply.github.com>
2021-10-25 21:15:42 +0900

    lesson４　課題再再提出
    順番に表示

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class Test : MonoBehaviour
+
 {
     // Use this for initialization
     void Start()
     {
         //配列を初期化する
-        int[] array = { 67, 10101, 77, 88, 99 };
+        int[] array = {30, 20, 50, 10, 80, 15, 60, 100};
         // 配列の要素をすべて表示する
-        for (int i = array.Length-1; i >= 0; i--)
+        for (int i = 0; i < array.Length; i++)
         {
             Debug.Log (array [i]);
         }
