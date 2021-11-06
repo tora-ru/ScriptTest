cfd8902982650058a058091a392edc5db82db8c8
tora-ru <91678534+tora-ru@users.noreply.github.com>
2021-11-06 22:37:33 +0900

    lesson４課題
    初期化

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,13 +9,16 @@ public class Test : MonoBehaviour
     void Start ()
     {
         //配列を初期化する
-        int[] array = {30, 20, 50, 10, 80, 15, 60, 100};
+        int[] array = {67, 10101, 66, 77, 88};
 
         //配列の分だけ処理を繰り返す
-        for (int i = array.Length-1; i >= 0; i--)
+        for (int i = 0; i < array.Length; i++)
         {
+            if (array[i] >= 50)
+            {
                 //配列の要素を表示する
-                Debug.Log (array [i]);
+                Debug.Log(array[i]);
+            }
         }
     }
 
